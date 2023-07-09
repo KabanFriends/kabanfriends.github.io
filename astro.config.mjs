4c0d759b2debc5e317a2893296be8837935d1d79
KabanFriends <yuukun2004@protonmail.ch>
2023-07-09 18:04:10 +0900

    Add astro-compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
 
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://kabanfriends.github.io'
+  site: 'https://kabanfriends.github.io',
+  integrations: [compress()]
 });
