19d691ac2f4798403b24633a40e348fb593e9bdb
KabanFriends <yuukun2004@protonmail.ch>
2023-07-09 14:09:36 +0900

    Add V4 files

---
+++ public/js/localize.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var localizeLang = undefined;
+var localizeElements = [];
+var originalTexts = {};
+var localizedTexts = {};
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (document.getElementsByClassName) {
+        var elems = document.getElementsByClassName("localize");
+        for (var i = 0; i < elems.length; i++) {
+            if (elems[i].id === "" || elems[i].id === undefined) {
+                console.error("MISSING LOCALIZATION ID!!");
+                console.error(elems);
+                continue;
+            }
+            localizeElements.push(elems[i].id);
+            originalTexts[elems[i].id] = elems[i].textContent;
+        }
+    }
+
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.getItem("localizeLang") != null) {
+            localize(localStorage.getItem("localizeLang"));
+        }
+    }
+});
+
+function unlocalize() {
+    localizeLang = undefined;
+    for (var i = 0; i < localizeElements.length; i++) {
+        var elems = document.querySelectorAll(`[id=${localizeElements[i]}]`).forEach((elem) => {
+            elem.textContent = originalTexts[localizeElements[i]];
+        });
+    }
+
+    if (typeof(Storage) !== "undefined") {
+        localStorage.removeItem("localizeLang");
+    }
+}
+
+function localize(lang) {
+    localizeLang = lang;
+    $.getJSON(`/lang/${lang}.json`, function(json) {
+        localizedTexts = json;
+        for (var i = 0; i < localizeElements.length; i++) {
+            if (localizeElements[i] in json) {
+                var elems = document.querySelectorAll(`[id=${localizeElements[i]}]`).forEach((elem) => {
+                    elem.textContent = json[localizeElements[i]];
+                });
+            } else {
+                console.error("MISSING LOCALIZATION KEY IN JSON!! " + localizeElements[i]);
+            }
+        }
+    });
+
+    if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("localizeLang", lang);
+    }
+}
---
+++ public/js/player.js
@@ -0,0 +1,39 @@
+let player;
+
+function start(name) {
+    let swfobject = {};
+    
+    swfobject.embedSWF = function(url, cont) {
+        let ruffle = window.RufflePlayer.newest();
+        
+        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
+            style: "width: 100%; height: 100%;"
+        });
+
+        player.config = {
+            letterbox: "on",
+            autoplay: "on",
+            unmuteOverlay: "hidden"
+        };
+
+        player.load({ url: url });
+    }
+
+    swfobject.embedSWF(`/swf/mildescape/${name}.swf`, 'ruffle');
+}
+
+function fullscreen() {
+    if (player === undefined) {
+        return;
+    }
+
+    try {
+        player.enterFullscreen();
+    } catch (err) {
+        console.error(err);
+        alert(localizeLang === undefined ? 
+            "フルスクリーン表示に失敗しました" :
+            localizedTexts["localize-mildescape-fullscreen-error"]
+        );
+    }
+}
