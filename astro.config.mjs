6b8406e676744a9985dfed338a63864abcbb1c1f
KabanFriends <yuukun2004@protonmail.ch>
2023-11-20 06:51:41 +0900

    Add UselessBot PP & TOS (WIP)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kabanfriends.github.io',
-  integrations: [compress()]
+  integrations: [compress(), mdx()]
 });
