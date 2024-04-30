c83cd1db2d15c6ee77f1f68827c384f6a7049689
KabanFriends <yuukun2004@protonmail.ch>
2024-05-01 00:57:41 +0900

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kabanfriends.github.io',
-  integrations: [compress(), mdx()]
+  integrations: [
+    compress(),
+    mdx(),
+    sitemap({
+      filter: (page) =>
+        !page.startsWith("https://kabanfriends.github.io/skin") &&
+        !page.startsWith("https://kabanfriends.github.io/test") &&
+        !page.startsWith("https://kabanfriends.github.io/mildescape/escape")
+    })
+  ]
 });
